af79bd77c10c450be2a5bcd1b375cde3a05f8e73
Thomas Wolff <moosehead96@hotmail.com>
2015-11-24 14:54:00 -0700

    The random sampler code was not updated

---
+++ pythonCourseRegix.py
@@ -0,0 +1,15 @@
+
+info = []
+infoNum = []
+
+import os
+import re
+ 
+os.chdir('C:\Users\U2970\Documents')
+for line in open('regex_sum_43.txt','rb'):
+    for line in re.findall('[0-9]*',line):
+            info.append(line)            
+for line in filter(None,info):
+    infoNum.append(int(line))
+print sum(infoNum)
+
