acbd4cff3200bbc2126a6f4db636f5569b3efecf
Thomas Wolff <moosehead96@hotmail.com>
2015-12-08 08:15:32 -0700

    Update pyplotData.py

--- pyplotData.py
+++ pyplotData.py
@@ -1,6 +1,7 @@
 import os
 import csv
 import glob as g
+import pprint as p
 import matplotlib.pyplot as plt
 
 os.chdir('C:\Users\U2970\Desktop\calibrationFiles')
@@ -21,13 +22,24 @@ def graphWriterIRIandRut():
             iriLList.append(float(col['IRI LWP ']))
             rutRList.append(float(col[' RUT R e']))
             rutLList.append(float(col[' RUT L e']))
-    plt.scatter([iriRList,iriLList],[rutRList,rutLList])        
-    fig1 = plt.gcf()
-    plt.xlabel('Rut value',fontsize=14)
-    plt.ylabel('IRI value',fontsize=14)
-    plt.title('IRI vs Rut data')
+            
+    plt.subplot(2, 1, 1)
     plt.grid(True)
-    plt.show(fig1)
+    plt.ylabel('IRI value',fontsize=14)
+    plt.title('IRI data shown per mile')
+    ax1 = plt.scatter([startList,endList],[iriRList,iriLList])
+    plt.tick_params(axis='both', which='major', labelsize=8)
+
+    plt.subplot(2, 1, 2)
+    plt.grid(True)
+    plt.xlabel('Mile marker',fontsize=14)
+    plt.ylabel('Rut value',fontsize=14)
+    plt.title('Rut data shown per mile')
+    ax2 = plt.scatter([startList,endList],[rutRList,rutLList])
+    plt.tick_params(axis='both', which='major', labelsize=8)
+
+    plt.gcf()
+    plt.show()
     plt.gcf().clear()
     plt.close('all')
 graphWriterIRIandRut()
