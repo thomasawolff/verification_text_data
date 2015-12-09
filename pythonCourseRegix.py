b321d4ce1d63019ece87f176a24fb48eed2c0132
Thomas Wolff <moosehead96@hotmail.com>
2015-12-09 08:38:02 -0700

    Update pythonCourseRegix.py

--- pythonCourseRegix.py
+++ pythonCourseRegix.py
@@ -1,11 +1,11 @@
-
-info = []
-infoNum = []
-
 import os
 import re
  
+info = []
+infoNum = []
+
 os.chdir('C:\Users\U2970\Documents')
+
 for line in open('regex_sum_43.txt','rb'):
     for line in re.findall('[0-9]*',line):
             info.append(line)            
